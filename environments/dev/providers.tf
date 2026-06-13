da374a7fc6598505482ba22e291e0c2606cbde94
Shivesh-shiva <shiveshec1032@gmail.com>
2026-06-13 22:03:35 +0530

    provider

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -6,13 +6,13 @@ terraform {
     }
   }
   backend "azurerm" {
-    use_oidc             = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
-    use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
-    tenant_id            = "07b6dcc9-18d6-45a6-b3d3-7183c906e2ba"  # Can also be set via `ARM_TENANT_ID` environment variable.
-    client_id            = "001d55f2-b456-4af2-b966-97580811f90e"  # Can also be set via `ARM_CLIENT_ID` environment variable.
-    storage_account_name = "shiveshstorage183"     # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "dev.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    use_oidc             = true
+    use_azuread_auth     = true
+    tenant_id            = "07b6dcc9-18d6-45a6-b3d3-7183c906e2ba"
+    client_id            = "001d55f2-b456-4af2-b966-97580811f90e"
+    storage_account_name = "shiveshstorage183"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
   }
 }
 
