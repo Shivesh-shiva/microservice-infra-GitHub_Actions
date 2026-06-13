6afb8b36963edbcccc1862040d7457984ea55561
Shivesh-shiva <shiveshec1032@gmail.com>
2026-06-13 21:33:12 +0530

    Add backend configuration for Azure in providers.tf

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "azurerm" {
+    storage_account_name = "shiveshstorage183"     # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
 
 provider "azurerm" {
