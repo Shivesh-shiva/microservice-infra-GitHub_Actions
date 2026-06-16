1e1b2c662de3ba4f9203599c749ad442c1f37485
Shivesh-shiva <shiveshec1032@gmail.com>
2026-06-17 01:07:28 +0530

    pip

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -6,7 +6,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "shivesh-rg"
+    use_oidc             = true                                    
+    use_azuread_auth     = true                                    
+    tenant_id            = "07b6dcc9-18d6-45a6-b3d3-7183c906e2ba"  
+    client_id            = "f4127f50-cb1a-4e11-9f9a-247acebb58f7"
     storage_account_name = "shiveshstorage183"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
@@ -16,7 +19,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "b107b989-85d9-4402-94e8-40ae932b8893"
-  tenant_id       = "07b6dcc9-18d6-45a6-b3d3-7183c906e2ba"
-  client_id       = "f4127f50-cb1a-4e11-9f9a-247acebb58f7"
 }
