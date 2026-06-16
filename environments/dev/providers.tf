e6c4723289b0a5446eb26e752d67e04c6bd5d06c
Shivesh-shiva <shiveshec1032@gmail.com>
2026-06-17 00:04:34 +0530

    updated provider

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -6,10 +6,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    use_oidc             = true
-    use_azuread_auth     = true
-    tenant_id            = "07b6dcc9-18d6-45a6-b3d3-7183c906e2ba"
-    client_id            = "001d55f2-b456-4af2-b966-97580811f90e"
+    resource_group_name  = "shivesh-rg"
     storage_account_name = "shiveshstorage183"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
